afd0e8449cc49708f00f18db1037bd303fe52038
Delay-n-days <67568581+Delay-n-days@users.noreply.github.com>
2020-07-28 09:52:47 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+while 1:
+    print("Hello VScode")
+    print("Hello VScode")
+    print("Hello VScode")
+    print("Hello VScode")
+    print("Hello VScode")
+    print("Hello VScode")
+    print("Hello VScode")
+    print("Hello VScode")
+    
---
+++ my.js
@@ -0,0 +1,3 @@
+//alert('hello world');
+document.write("Helloworld");
+//var age //ÉùÃ÷±äÁ¿
